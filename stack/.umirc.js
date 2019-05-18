8e0e94ef3bacfd39f917143611797f997b7061a2
lsqaisen <lai413633@outlook.com>
2019-05-18 19:36:08 +0800

    add stack model

--- stack/.umirc.js
+++ stack/.umirc.js
@@ -1,21 +1,28 @@
-// import '../../mife/bin/index'
+const { NODE_ENV } = process.env;
 
-// console.log(require('mife'))
 export default {
+  history: 'hash',
   plugins: [
     ['umi-plugin-react', {
       dva: true,
+      antd: {
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": true // `style: true` 会加载 less 文件
+      },
       routes: {
         exclude: [
           /model/,
+          /basic/
         ],
       },
       dynamicImport: {
-        webpackChunkName: true
+        webpackChunkName: true,
+        loadingComponent: null,
       },
     }],
     ['mife', {
-      type: 'plugin',
+      type: NODE_ENV === "development" ? 'portal' : 'plugin',
       dynamicImport: true,
       publicPath: '/lib/',
       externals: {
@@ -23,7 +30,59 @@ export default {
         'react-dom': 'window.ReactDOM',
         'dva': 'window.dva',
       },
-    }]
+    }],
   ],
   hash: true,
+  alias: {
+    '@': './src/components/'
+  },
+  define: {
+    "process.env.OEM_NAME": '/kubeup',
+    "process.env.VERSION": new Date().getTime(),
+  },
+  theme: {
+    "primary-color": "#1557fb"
+  },
+  chainWebpack(config, { webpack }) {
+    config.resolve.extensions
+      .add(".tsx")
+      .prepend(".tsx");
+    config.resolve.extensions
+      .add(".ts")
+      .prepend(".ts");
+  },
+  proxy: {
+    "/lib/login": {
+      "target": "http://localhost:5000",
+      "changeOrigin": true,
+      "pathRewrite": { "^/lib/login": "" }
+    },
+    //oem
+    "/static/oem": {
+      "target": "http://localhost:5000/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/static/oem": "/static/oem/kubeup" }
+    },
+    // api
+    "/api": {
+      "target": "http://192.168.1.60:30000/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api": "/api" }
+    },
+    "/login": {
+      "target": "http://192.168.1.60:30000/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/login": "/login" }
+    },
+    "/logout": {
+      "target": "http://192.168.1.60:30000/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/logout": "/logout" }
+    },
+    "/profile": {
+      "target": "http://192.168.1.60:30000/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/profile": "/profile" }
+    },
+  },
 }

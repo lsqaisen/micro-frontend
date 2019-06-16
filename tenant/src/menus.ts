35eb9a1dbce7433f9528bea2022b473a0da00794
lsqaisen <lai413633@outlook.com>
2019-06-16 23:05:20 +0800

    tenant

---
+++ tenant/src/menus.ts
@@ -0,0 +1,38 @@
+const menus = {
+  admin: [{
+    key: "list",
+    name: "所有空间",
+    path: "/list"
+  }, {
+    key: "default",
+    name: "默认配额",
+    path: "/default"
+  }, {
+    key: "charging",
+    name: "计费定价",
+    path: "/charging"
+  }, {
+    key: "qrcharge",
+    name: "确认充值",
+    path: "/qrcharge"
+  }, {
+    key: "bill",
+    name: "账单",
+    path: "/bill"
+  }, {
+    key: "set",
+    name: "账户设置",
+    path: "/set"
+  }],
+  user: [{
+    key: "bill",
+    name: "账单",
+    path: "/bill"
+  }, {
+    key: "qrcharge",
+    name: "确认充值",
+    path: "/qrcharge"
+  }]
+}
+
+export default menus;
---
+++ tenant/typings.d.ts
@@ -0,0 +1,18 @@
+declare module '*.less';
+declare module 'react-dom';
+declare module 'react-lifecycles-compat';
+declare module 'mife/bin/api';
+declare module 'rc-animate';
+declare module 'inputmask';
+declare module 'lodash.debounce';
+declare function MODEL(namspace: string): string;
+
+interface Window {
+  mife_menus?: object;
+  Number: any;
+  sider_drawers: {
+    [key: string]: any;
+  };
+}
+
+declare var window: Window;

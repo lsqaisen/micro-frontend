628862045a3cac846a4e06a55cc1a030312bdaa0
lsqaisen <lai413633@outlook.com>
2019-06-20 14:57:38 +0800

    add

--- tenant/.umirc.js
+++ tenant/.umirc.js
@@ -50,8 +50,8 @@ export default {
     "error-color": "#ff5242",                                 // 错误色
     "font-size-base": "14px",                                 // 主字号
     "heading-color": "rgba(0, 0, 0, .85)",                    // 标题色
-    "text-color": "rgba(0, 0, 0, .65)",                       // 主文本色
-    "text-color-secondary": "rgba(0, 0, 0, .45)",             // 次文本色
+    "text-color": "#2f2f2f",                       // 主文本色
+    "text-color-secondary": "#888888",                        // 次文本色
     "disabled-color": "rgba(0, 0, 0, .25)",                  // 失效色
     "border-radius-base": "4px",                              // 组件/浮层圆角
     "border-color-base": "#d9d9d9",                           // 边框色

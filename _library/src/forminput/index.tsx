b504bdb8c663a7b5803a5f5f3cb6d13a9695e636
lsqaisen <lai413633@outlook.com>
2019-07-04 23:40:45 +0800

    library add et

---
+++ _library/src/forminput/index.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { PureComponent } from 'react';
+import { Form } from 'antd';
+import { FormItemProps, FormComponentProps, FormCreateOption } from 'antd/lib/form';
+import { generateUUID } from '../../utils/'
+
+export interface FormInputItemProps extends FormItemProps {
+  children: React.ReactNode,
+}
+
+export interface FormInputProps<T = any> extends FormComponentProps {
+  value?: T;
+  onChange?: (v: T) => void;
+}
+
+export const FormInputItem = (props: FormInputItemProps) => {
+  return (
+    <Form.Item
+      style={{ marginBottom: 0 }}
+      validateStatus=""
+      help=""
+      {...props}
+    >{props.children}</Form.Item>
+  )
+}
+
+export default (options?: FormCreateOption<any>) => <T extends Object = {}>(WrappedComponent: React.ComponentClass<any, any>) => {
+  class FormInput extends PureComponent<FormInputProps<T>, any> {
+    changeValidator = (rules: any[]) => {
+      rules.push({
+        validator: (rule: any, value: any, callback: any) => {
+          !!this.props.form.validateFieldsAndScroll && this.props.form.validateFieldsAndScroll((error: any, _: any) => {
+            if (error) {
+              let errors = Object.values(error).
+                map(({ errors }: any) => {
+                  return errors
+                    .map((error: any) => error.message)
+                    .join(';')
+                });
+              callback(Array.from(new Set(errors)))
+            }
+            callback()
+          })
+        }
+      })
+    }
+    UNSAFE_componentWillReceiveProps(nextProps: any) {
+      if (!(nextProps as any)['data-__meta'].rules) {
+        (nextProps as any)['data-__meta'].rules = [];
+      }
+      this.changeValidator((nextProps as any)['data-__meta'].rules);
+    }
+    componentDidMount() {
+      if (!(this.props as any)['data-__meta'].rules) (this.props as any)['data-__meta'].rules = [];
+      this.changeValidator((this.props as any)['data-__meta'].rules);
+    }
+    render() {
+      return React.createElement(WrappedComponent, this.props);
+    }
+  }
+
+  return Form.create<FormInputProps<T>>({
+    name: `${generateUUID()}`,
+    onValuesChange: ({ onChange }, _, allValues) => {
+      onChange && onChange(allValues as T);
+    },
+    ...(options || {}),
+  })(FormInput as any);
+};
--- login/.umirc.js
+++ login/.umirc.js
@@ -1,115 +1,118 @@
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
 
 const { NODE_ENV } = process.env;
-const antdFiles = JSON.parse(`{${fs.readFileSync(__dirname + '/node_modules/antd/es/index.d.ts')
-  .toString()
-  .split(';')
-  .filter(v => !!v && v != '\n' && v != '\n\r')
-  .map(v => v.replace(/export { default as (.+) } from '\.\/(.+)'/g, '"antd/es/$2":"window.antd.$1"').replace(/ /g, ''))
-  .join(',')}}`);
+const antdFiles = JSON.parse(
+  `{${fs
+    .readFileSync(__dirname + "/node_modules/antd/es/index.d.ts")
+    .toString()
+    .split(";")
+    .filter(v => !!v && v != "\n" && v != "\n\r")
+    .map(v =>
+      v
+        .replace(
+          /export { default as (.+) } from '\.\/(.+)'/g,
+          '"antd/es/$2":"window.antd.$1"'
+        )
+        .replace(/ /g, "")
+    )
+    .join(",")}}`
+);
 
 export default {
-  history: 'hash',
-  publicPath: '/static/dist/',
-  outputPath: '/lib/',
+  history: "hash",
+  publicPath: "/static/dist/",
+  outputPath: "/lib/",
   plugins: [
-    ['umi-plugin-react', {
+    ["umi-plugin-react", {
       dva: true,
       antd: {
-        "libraryName": "antd",
-        "libraryDirectory": "es",
-        "style": true // `style: true` 会加载 less 文件
+        libraryName: "antd",
+        libraryDirectory: "es",
+        style: true // `style: true` 会加载 less 文件
       },
       routes: {
-        exclude: [
-          /model/,
-          /basic/
-        ],
+        exclude: [/model/, /basic/]
       },
       dynamicImport: {
         webpackChunkName: true,
-        loadingComponent: null,
-      },
+        loadingComponent: null
+      }
     }],
-    [path.join(__dirname, '../config/bin/'), {
-      type: NODE_ENV === "development" ? 'portal' : 'plugin',
+    [path.join(__dirname, "../_config/bin/"), {
+      type: NODE_ENV === "development" ? "portal" : "plugin",
       dynamicImport: true,
-      publicPath: '/service/login/lib/',
-      scripts: [
-        'react.js',
-        'react-dom.js',
-        'dva.js',
-        'antd.min.js',
-      ],
+      publicPath: "/service/login/lib/",
       externals: [{
-        'react': 'window.React',
-        'react-dom': 'window.ReactDOM',
-        'dva': 'window.dva',
-        'antd': 'window.antd',
+        react: "window.React",
+        "react-dom": "window.ReactDOM",
+        dva: "window.dva",
+        antd: "window.antd",
         ...antdFiles
       }],
-    }],
+      scripts: [
+        "react.js",
+        "react-dom.js",
+        "dva.js",
+        "antd.min.js"
+      ],
+    }]
   ],
   hash: true,
   alias: {
-    '@': './src/components/'
+    "@": "./src/components/"
   },
   define: {
-    'MODEL': 'login',
-    "process.env.OEM_NAME": NODE_ENV === "development" ? '/kubeup' : '',
-    "process.env.VERSION": new Date().getTime(),
+    MODEL: "login",
+    "process.env.OEM_NAME": NODE_ENV === "development" ? "/kubeup" : "",
+    "process.env.VERSION": new Date().getTime()
   },
   theme: {
-    "primary-color": "#286cff",                               // 全局主色
-    "link-color": "#286cff",                                  // 链接色
-    "success-color": "#0db46e",                               // 成功色
-    "warning-color": "#ff9000",                                // 警告色
-    "error-color": "#ff5242",                                 // 错误色
-    "font-size-base": "14px",                                 // 主字号
-    "heading-color": "rgba(0, 0, 0, .85)",                    // 标题色
-    "text-color": "#2f2f2f",                       // 主文本色
-    "text-color-secondary": "#888888",                        // 次文本色
-    "disabled-color": "rgba(0, 0, 0, .25)",                  // 失效色
-    "border-radius-base": "4px",                              // 组件/浮层圆角
-    "border-color-base": "#d9d9d9",                           // 边框色
-    "box-shadow-base": "0 2px 8px rgba(0, 0, 0, .15)",        // 浮层阴影
-    "sider-background-color": "#f2f7fb",                      // 菜单背景颜色
+    "primary-color": "#286cff", // 全局主色
+    "link-color": "#286cff", // 链接色
+    "success-color": "#0db46e", // 成功色
+    "warning-color": "#ff9000", // 警告色
+    "error-color": "#ff5242", // 错误色
+    "font-size-base": "14px", // 主字号
+    "heading-color": "rgba(0, 0, 0, .85)", // 标题色
+    "text-color": "#2f2f2f", // 主文本色
+    "text-color-secondary": "#888888", // 次文本色
+    "disabled-color": "rgba(0, 0, 0, .25)", // 失效色
+    "border-radius-base": "4px", // 组件/浮层圆角
+    "border-color-base": "#d9d9d9", // 边框色
+    "box-shadow-base": "0 2px 8px rgba(0, 0, 0, .15)", // 浮层阴影
+    "sider-background-color": "#f2f7fb" // 菜单背景颜色
   },
   chainWebpack(config, { webpack }) {
-    config.resolve.extensions
-      .add(".tsx")
-      .prepend(".tsx");
-    config.resolve.extensions
-      .add(".ts")
-      .prepend(".ts");
+    config.resolve.extensions.add(".tsx").prepend(".tsx");
+    config.resolve.extensions.add(".ts").prepend(".ts");
   },
   proxy: {
     // api
     "/api": {
-      "target": "http://192.168.1.181:30000/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/api": "/api" }
+      target: "http://192.168.1.181:30000/",
+      changeOrigin: true,
+      pathRewrite: { "^/api": "/api" }
     },
     "/login": {
-      "target": "http://192.168.1.181:30000/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/login": "/login" }
+      target: "http://192.168.1.181:30000/",
+      changeOrigin: true,
+      pathRewrite: { "^/login": "/login" }
     },
     "/logout": {
-      "target": "http://192.168.1.181:30000/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/logout": "/logout" }
+      target: "http://192.168.1.181:30000/",
+      changeOrigin: true,
+      pathRewrite: { "^/logout": "/logout" }
     },
     "/profile": {
-      "target": "http://192.168.1.181:30000/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/profile": "/profile" }
+      target: "http://192.168.1.181:30000/",
+      changeOrigin: true,
+      pathRewrite: { "^/profile": "/profile" }
     },
     "/service": {
-      "target": "http://192.168.1.181:30000/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/service": "/service" }
-    },
-  },
-}
+      target: "http://192.168.1.181:30000/",
+      changeOrigin: true,
+      pathRewrite: { "^/service": "/service" }
+    }
+  }
+};
